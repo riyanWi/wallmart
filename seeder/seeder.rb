61ef917f7aac030aefe577580551c4a93bbe04fe
Riyan Wibowo <tunggalrahayu1@gmail.com>
2022-03-03 02:53:09 +0700

    add more headers on seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,8 +2,8 @@ pages << {
     page_type: "listings",
     method: "GET",
     headers: {
-        "user-agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
         "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"'
     },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
     fetch_type: "browser",
