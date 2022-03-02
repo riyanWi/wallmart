98824e5d25d9585425bc709b4b11bdf4d37b0ea5
Riyan Wibowo <tunggalrahayu1@gmail.com>
2022-03-03 02:01:29 +0700

    add more headers on seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,7 +1,12 @@
 pages << {
     page_type: "listings",
     method: "GET",
-    headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+    headers: {
+        "user-agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "cookie" => page['response_cookie']
+    },
     url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
-    fetch_type: "browser"
+    fetch_type: "browser",
+    
   }
